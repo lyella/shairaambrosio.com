ac8f40a45062fc7be5950e922430b020d4b50d13
lyella <ambrosioshaira@gmail.com>
2024-05-25 19:31:24 +0800

    Updated the Experience list

--- data/experience.ts
+++ data/experience.ts
@@ -8,28 +8,35 @@ type Position = {
 
 export const allPositions: readonly Position[] = [
   {
-    id: 1,
+    id: 5,
+    position: "Front-end Developer",
+    company: "ZZ",
+    date: "04.2023 - present",
+    link: "",
+  },
+  {
+    id: 4,
     position: "Front-end Developer",
     company: "Playgroundzero",
-    date: "09.2022 - present",
+    date: "09.2022 - 01.2023",
     link: "https://www.playgroundzero.com/",
   },
   {
-    id: 2,
+    id: 3,
     position: "Front-end Engineer",
     company: "Upward Next Inc.",
     date: "10.2020 - 05.2022",
     link: "https://www.upwardnext.com/",
   },
   {
-    id: 3,
+    id: 2,
     position: "Blogger",
     company: "shairaambrosio.com",
     date: "02.2023 - present",
     link: "https://shairaambrosio.com",
   },
   {
-    id: 4,
+    id: 1,
     position: "Freelancer",
     company: "None",
     date: "10.2018 - Present",
